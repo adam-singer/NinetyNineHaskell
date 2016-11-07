f0f79fa0a14a72d5453b6b05cc9a206575f6e118
Adam Singer <financeCoding@gmail.com>
2016-11-06 20:46:03 -0800

    adding other implementations of last

--- src/H99_01.hs
+++ src/H99_01.hs
@@ -14,6 +14,7 @@ Prelude> myLast ['x','y','z']
 -}
 
 myLast :: [a] -> a
+myLast [] = error "list is empty"
 myLast (x:[]) = x
 myLast (x:xs) = myLast xs
 
@@ -36,4 +37,57 @@ it :: Num b => b
 myLast' :: [a] -> a
 myLast' = foldr1 (const id)
 
-solution01 = undefined
+myLast'':: [a] -> a
+myLast'' = head . reverse
+
+{-|
+λ > :t const
+const :: a -> b -> a
+λ > :t flip
+flip :: (a -> b -> c) -> b -> a -> c
+λ > :t foldr1
+foldr1 :: Foldable t => (a -> a -> a) -> t a -> a
+λ > :t flip const
+flip const :: b -> c -> c
+λ > :t foldr1 (flip const)
+foldr1 (flip const) :: Foldable t => t b -> b
+λ > :t foldr1 (flip const) [1,2,3]
+foldr1 (flip const) [1,2,3] :: Num b => b
+λ > foldr1 (flip const) [1,2,3]
+    3
+it :: Num b => b
+-}
+
+myLast''' :: [a] -> a
+myLast''' = foldr1 (flip const)
+
+{-|
+
+λ > :t snd
+snd :: (a, b) -> b
+λ > :t curry
+curry :: ((a, b) -> c) -> a -> b -> c
+λ > :t foldl1
+foldl1 :: Foldable t => (a -> a -> a) -> t a -> a
+λ > :t foldl1 (curry snd)
+foldl1 (curry snd) :: Foldable t => t a -> a
+λ > foldl1 (curry snd) [1,2,3]
+    3
+it :: Num a => a
+
+-}
+
+myLast'''' :: [a] -> a
+myLast'''' = foldl1 (curry snd)
+
+{-|
+
+-}
+
+myLast''''' :: [a] -> a
+myLast''''' [] = error "list is empty"
+myLast''''' xs = xs !! (length xs - 1)
+
+myLast'''''' :: [a] -> a
+myLast'''''' = last
+
